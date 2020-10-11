f315f385adbc10314988a4fa2982160a2c3ea877
osobiehl <osobiehl@me.com>
2020-10-11 23:55:22 +0200

    made a very bad database populating script jaJA

--- populate_db.py
+++ populate_db.py
@@ -29,7 +29,7 @@ else:
 cursor = connection.cursor();
 clubs = open('CLUB_NAMES')
 lines = clubs.readlines()
-'''
+
 for line in lines:
     club_info = line.split(' ', 1)
     cursor.execute("INSERT INTO club (name, description) VALUES (%s, %s)", (club_info[0], club_info[1]))
@@ -41,7 +41,6 @@ for i in range(0, 16):
             cursor.execute("INSERT INTO subscription (user_id, club_id) VALUES (%s, %s)", (i, random.randint(0,7)))
         except Exception:
             continue
-'''
 
 for i in range(1, 18):
     try:
@@ -61,6 +60,35 @@ for i in range(1, 18):
                                random.randint(0,17),
                                "USER: "+user_name+", title!",
                                "This is "+club_name+" this is my content, in club: "+club_name)
+
+                           )
+    except Exception as err:
+        print(err)
+        print(err.__doc__)
+        continue
+
+        #user id: 5, club id: 3Error Code: 1055. Error Code: 1305. FUNCTION dbws.DATEADD does not exist
+
+
+
+for i in range(1, 18):
+    try:
+        cursor.execute(
+            '''SELECT u.name, c.name, p.post_id from subscription s
+            INNER JOIN club c on s.user_id = %s AND c.club_id = s.club_id 
+            INNER JOIN user u ON u.user_id = s.user_id 
+            INNER JOIN post p on p.club_id = s.club_id ''',
+            (i, ))
+        res = cursor.fetchall()
+
+        for (user_name, club_name, post_id) in res:
+            cursor.execute("INSERT INTO comments (user_id, post_id, date, likes,  content) VALUES (%s, %s, %s, %s, %s)",
+                           (
+                               i,
+                               post_id,
+                               datetime.datetime.utcnow() - datetime.timedelta(hours=random.randint(1,2), minutes=random.randint(0,60)),
+                               random.randint(0,17),
+                               "hello! I am "+user_name+", commenting on this post from club:" + club_name)
 
                            )
     except Exception as err:
