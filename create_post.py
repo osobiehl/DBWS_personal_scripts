5ee70d8bb12b5198af8fe4acdaac5a628ad99a53
osobiehl <osobiehl@me.com>
2020-10-23 18:09:33 +0200

    more changes

--- create_post.py
+++ create_post.py
@@ -13,7 +13,7 @@ from email import message
 from email.mime import multipart
 from email.mime import nonmultipart
 from email.mime import text
-
+# TODO add error handling on input JSON
 cgitb.enable()
 print("Content-Type: text/html;charset=utf-8\n\n")
 a = sys.stdin.read()
@@ -21,9 +21,16 @@ req_content = json.loads(a)
 img_b64 = req_content['image']
 img = b64decode(img_b64)
 req_content['image'] = None
-print (req_content)
-
-f = open('test.jpeg', 'wb+')
+options = {
+    "image/gif": "gif",
+    "image/jpeg": "jpeg",
+    "image/png": "png"
+}
+if req_content['image-type'] not in options:
+    print("type not supported!")
+print(req_content)
+# make proper file w/ proper format
+f = open('test.' + options[req_content['image-type']], 'wb+')
 f.write(img)
 
 f.close()
